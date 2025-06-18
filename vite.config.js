c9de236abc2c67f5bbc091b26461bbe0d779190d
Naomi Guyer <naomi.guyer@resn.co.nz>
2025-06-19 09:50:52 +1200

    FIX: Make sure compiled code has access to process

--- vite.config.js
+++ vite.config.js
@@ -1,87 +1,101 @@
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from 'vite'
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
+import process from 'node:process'
 
 import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@/": `${path.resolve(__dirname, "src")}/`,
-      "~/": `${path.resolve(__dirname, "src")}/`,
-      "gsap/": `${path.resolve(__dirname, "node_modules/gsap/esm")}/`
-    },
-  },
+export default async mode => {
+  Object.assign(process.env, loadEnv(mode.mode, process.cwd()))
 
-  publicDir: 'public',
-  root: '.',
-  base: '/',
+  const env = loadEnv(mode, process.cwd(), '')
 
 
-  assetsInclude: [
-    "**/*.gltf",
-    "**/*.glb",
-    "**/*.hdr",
-    "**/*.exr",
-    "**/*.jpg",
-    "**/*.jpeg",
-    "**/*.png",
-    "**/*.gif",
-    "**/*.svg",
-    "**/*.webp"
-  ],
+  const dropLogs = []
+  // const dropLogs = mode.mode === 'development' ? [] : ['console.log', 'console.trace', 'console.debug']
 
-
-  plugins: [
-    vue(),
-  ],
-  build: {
-    outDir: "dist",
-    manifest: true,
-    cssCodeSplit: true,
-    assetsDir: 'assets',
-    lib: {
-      entry: path.resolve(__dirname, "src/main.js"),
-      name: "SharedComponents",
-      fileName: (format) => `bundle.js`,
-      formats: ["es"],
-    },
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true, // Forces everything into a single file
-        manualChunks: undefined // Prevents code splitting
-      },
-      external: [
-        /public\/webflow\/.*/
-      ]
-
-    },
-  },
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: ` 
-            @use "sass:math";
-            @use "sass:color";
-            @use "sass:map";
-        `,
+  return defineConfig({
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`,
+        "~/": `${path.resolve(__dirname, "src")}/`,
+        "gsap/": `${path.resolve(__dirname, "node_modules/gsap/esm")}/`,
       },
     },
-  },
-  server: {
-    port: 3333,
-    strictPort: true,
-    host: true,
-    fs: {
-      strict: false,
-      allow: ['..']
+    define: {
+      'process': process,
+    },
+
+    publicDir: 'public',
+    root: '.',
+    base: '/',
+
+    esbuild: {
+      pure: dropLogs
+    },
+    assetsInclude: [
+      "**/*.gltf",
+      "**/*.glb",
+      "**/*.hdr",
+      "**/*.exr",
+      "**/*.jpg",
+      "**/*.jpeg",
+      "**/*.png",
+      "**/*.gif",
+      "**/*.svg",
+      "**/*.webp"
+    ],
+
+
+    plugins: [
+      vue(),
+    ],
+    build: {
+      outDir: "dist",
+      manifest: true,
+      cssCodeSplit: true,
+      assetsDir: 'assets',
+      lib: {
+        entry: path.resolve(__dirname, "src/main.js"),
+        name: "SharedComponents",
+        fileName: (format) => `bundle.js`,
+        formats: ["es"],
+      },
+      rollupOptions: {
+        output: {
+          inlineDynamicImports: true, // Forces everything into a single file
+          manualChunks: undefined // Prevents code splitting
+        },
+        external: [
+          /public\/webflow\/.*/
+        ]
+
+      },
+    },
+
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: ` 
+              @use "sass:math";
+              @use "sass:color";
+              @use "sass:map";
+          `,
+        },
+      },
+    },
+    server: {
+      port: 3333,
+      strictPort: true,
+      host: true,
+      fs: {
+        strict: false,
+        allow: ['..']
+      }
+
     }
 
-  }
-
-
-
-});
+  })
+}
